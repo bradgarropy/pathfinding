09ff214694a09461d0cfd5d90ba33b92c60d32a1
Brad Garropy <bradgarropy@gmail.com>
2015-04-05 22:01:51 -0500

    TODO#5: Develop grid creation method
    Stubbed out the grid creation method and arguments and included a
    docstring.

--- grid.py
+++ grid.py
@@ -1,1 +1,12 @@
 # grid.py
+
+def create_grid(width, height):
+	"""
+	Creates and initializes a grid of the provided length and width.
+
+	width	-- Width of the grid.
+	height	-- Height of the grid.
+	"""
+
+	pass
+	
