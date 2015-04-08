2d2cc9db26d420494d406e9230b0dba5fd1503e1
Brad Garropy <bradgarropy@gmail.com>
2015-04-07 22:32:06 -0500

    TODO#6: Develop grid printing method
    Cleaned up docstrings for the create_grid method. Implemented the
    draw_grid method.

--- grid.py
+++ grid.py
@@ -15,8 +15,8 @@ def create_grid(width, height):
     """
     Creates and initializes a grid of the provided length and width.
 
-    width	-- Width of the grid.
-    height	-- Height of the grid.
+    width	-- Width of the grid
+    height	-- Height of the grid
 
     Returns a list of lists representing a grid. All values are initialized to
     zero. The grid can be accessed by using the following notation:
@@ -25,9 +25,9 @@ def create_grid(width, height):
 
     You can visualize the graph as shown below. The origin is at the top left.
 
-    [1, 2, 3]
-    [4, 5, 6]
-    [7, 8, 9]
+        [1, 2, 3]
+        [4, 5, 6]
+        [7, 8, 9]
 
     In the example grid shown above, grid[0][0] would return 1.
     """
@@ -37,4 +37,46 @@ def create_grid(width, height):
 
     return grid
 
-# TODO Implement draw_grid(grid)
+
+def draw_grid(grid):
+    """
+    Prints the given grid and its values to standard out.
+
+    grid -- A list of lists representing a grid
+
+    Returns nothing. Take the example grid listed below.
+
+        grid=[[1,2,3],
+              [4,5,6],
+              [7,8,9]]
+
+    This grid would be printed as follows.
+
+        -------------
+        | 1 | 2 | 3 |
+        |---|---|---|
+        | 4 | 5 | 6 |
+        |---|---|---|
+        | 7 | 8 | 9 |
+        -------------
+    """
+
+    # calculate the width and height of the grid
+    width = len(grid[0])
+    height = len(grid)
+
+    # print top bar
+    print "----" * width + "-"
+
+    # iterate over rows and columns
+    for row in range(height):
+        for column in range(width):
+            print("| %d" % grid[row][column]),
+        print "|"
+
+        # print row separator unless it's the last row
+        if row != (height - 1):
+            print "|---" * width + "|"
+
+    # print bottom bar
+    print "----" * width + "-"
