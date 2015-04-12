c6990fc31cbba8fa5b999e49f20fadc047232380
Brad Garropy <bradgarropy@gmail.com>
2015-04-12 11:31:49 -0500

    TODO#15: Implement pathfinding algorithm
    Adjust algorithm to skip impassable nodes.

--- a_star.py
+++ a_star.py
@@ -30,20 +30,21 @@ def a_star(start, end):
         # explore the current node's neighbors
         for neighbor in current.neighbors():
 
-            # calculate the cost to reach the neighbor
-            new_cost = cost[current] + 1
+            # only consider the node if it is passable
+            if neighbor.passable():
 
-            # TODO: Cost should handle obstacles
+                # calculate the cost to reach the neighbor
+                new_cost = cost[current] + 1
 
-            # add new neighbors or update better neighbors
-            if (neighbor not in cost) or (new_cost < cost[neighbor]):
+                # add new neighbors or update better neighbors
+                if (neighbor not in cost) or (new_cost < cost[neighbor]):
 
-                # add cost and parent information
-                cost[neighbor] = new_cost
-                parent[neighbor] = current
+                    # add cost and parent information
+                    cost[neighbor] = new_cost
+                    parent[neighbor] = current
 
-                # place in appropriate spot in the frontier
-                priority = new_cost + neighbor.distance(node=end)
-                frontier.put(neighbor, priority)
+                    # place in appropriate spot in the frontier
+                    priority = new_cost + neighbor.distance(node=end)
+                    frontier.put(neighbor, priority)
 
     return parent
